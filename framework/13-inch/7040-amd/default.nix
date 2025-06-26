529975a2acd94cf33be588104747ba54a4d0ddf6
Minionflo <git@minionflo.net>
2025-06-26 15:32:03 +0200

    add option to disable wake on touch for framework 13 7040 amd

--- framework/13-inch/7040-amd/default.nix
+++ framework/13-inch/7040-amd/default.nix
@@ -22,16 +22,28 @@ in
         https://community.frame.work/t/tracking-framework-amd-ryzen-7040-series-lid-wakeup-behavior-feedback/39128/45
       '';
     };
+    hardware.framework.amd-7040.preventWakeOnTouchpad = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = ''
+        Stop the system from waking from suspend when the touchpad is clicked.
+
+        See:
+        https://community.frame.work/t/tracking-framework-amd-ryzen-7040-series-lid-wakeup-behavior-feedback/39128/104
+      '';
+    };
   };
 
   config = {
     # Workaround applied upstream in Linux >=6.7 (on BIOS 03.03)
     # https://github.com/torvalds/linux/commit/a55bdad5dfd1efd4ed9ffe518897a21ca8e4e193
-    services.udev.extraRules = lib.mkIf (lib.versionOlder config.boot.kernelPackages.kernel.version "6.7" && cfg.preventWakeOnAC) ''
+    services.udev.extraRules = lib.optionalString (lib.versionOlder config.boot.kernelPackages.kernel.version "6.7" && cfg.preventWakeOnAC) ''
       # Prevent wake when plugging in AC during suspend. Trade-off: keyboard wake disabled. See:
       # https://community.frame.work/t/tracking-framework-amd-ryzen-7040-series-lid-wakeup-behavior-feedback/39128/45
       ACTION=="add", SUBSYSTEM=="serio", DRIVERS=="atkbd", ATTR{power/wakeup}="disabled"
-    '';
+    '' + (lib.optionalString (cfg.preventWakeOnTouchpad) ''
+      ACTION=="add", SUBSYSTEM=="i2c", DRIVERS=="i2c_hid_acpi", ATTRS{name}=="PIXA3854:00", ATTR{power/wakeup}="disabled"
+    '');
 
     hardware.framework.laptop13.audioEnhancement.rawDeviceName = lib.mkDefault "alsa_output.pci-0000_c1_00.6.analog-stereo";
   };
